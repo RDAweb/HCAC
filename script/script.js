37d4d242f7d7a461680d566a51c978804db95d11
rg <ravindergod001tbdl@gmail.com>
2024-10-18 02:24:51 +0530

    Add files via upload

--- script/script.js
+++ script/script.js
@@ -9,63 +9,16 @@ let currentIndex = 0;
         }
         slides.style.transform = `translateX(-${currentIndex * 100}%)`;
     }
-
     setInterval(showNextSlide, 5000);
     // Smooth scroll effect for the entire website
     document.querySelectorAll('a[href^="#"]').forEach(anchor => {
         anchor.addEventListener('click', function (e) {
             e.preventDefault();
-
             document.querySelector(this.getAttribute('href')).scrollIntoView({
                 behavior: 'smooth'
             });
         });
     });
-
-    // Reveal elements on scroll
-    function revealOnScroll() {
-        var reveals = document.querySelectorAll(".reveal");
-        
-        for (var i = 0; i < reveals.length; i++) {
-            var windowHeight = window.innerHeight;
-            var elementTop = reveals[i].getBoundingClientRect().top;
-            var elementVisible = 150;
-            
-            if (elementTop < windowHeight - elementVisible) {
-                reveals[i].classList.add("active");
-            } else {
-                reveals[i].classList.remove("active");
-            }
-        }
-    }
-    window.addEventListener('resize', updateSlider);
-    window.addEventListener("scroll", revealOnScroll);
-
-    const profileCont = document.querySelector('.profile-cont');
-const cards = document.querySelectorAll('.profile-card');
-const prevBtn = document.querySelector('.prev');
-const nextBtn = document.querySelector('.next');
-
-let setIndex = 0;
-
-function updateSlider() {
-    const cardWidth = cards[0].offsetWidth + 400; // Width of card plus margin
-    profileCont.style.transform = `translateX(-${setIndex * cardWidth}px)`;
-}
-
-prevBtn.addEventListener('click', () => {
-    if (setIndex > 0) {
-        setIndex--;
-    }
-    updateSlider();
-});
-
-nextBtn.addEventListener('click', () => {
-    if (setIndex < cards.length - 1) {
-        setIndex++;
-    }
-    updateSlider();
-});
         // Function to check if an element is in the viewport
         function isInViewport(element) {
             const rect = element.getBoundingClientRect();
@@ -73,7 +26,6 @@ nextBtn.addEventListener('click', () => {
                 rect.top < window.innerHeight && rect.bottom >= 0
             );
         }
-    
         // Function to add the 'visible' class when an element is in view
         function handleScroll() {
             const sections = document.querySelectorAll('table');
